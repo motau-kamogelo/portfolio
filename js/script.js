a1e4826995442baa0575635bb15b117a5af12751
Kamogelo Motau <127486770+motau-kamogelo@users.noreply.github.com>
2024-10-07 20:30:14 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 document.getElementById('hamburger').addEventListener('click', function () {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
